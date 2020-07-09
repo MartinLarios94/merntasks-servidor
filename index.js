8b5d64b3d6042653a02fb60addd77ddec0701727
Martín Larios <13221820+MartinLarios94@users.noreply.github.com>
2020-07-08 22:15:11 -0600

    cambios en el config de node

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use( express.json({ extend: true }));
 
 // puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 // Importar Rutas
 app.use('/api/usuarios', require('./routes/usuarios'))
@@ -25,7 +25,7 @@ app.use('/api/proyectos', require('./routes/proyectos'))
 app.use('/api/tareas', require('./routes/tareas'))
 
 // arrancar la app
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${PORT}`);
     
 })
